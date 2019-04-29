03f08e70e69b016036d206e1b1663bd425fe683d
Hector Garcia <hectoregarciap@hotmail.com>
2019-04-29 09:01:59 -0500

    actualizando librerias

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Navbar, Grid, Row, Col } from 'react-bootstrap';
-import ProductList from './components/ProductList';
-import ShoppingCart from './components/ShoppingCart';
-import './App.css';
+import React, { Component } from "react";
+import { Navbar, Grid, Row, Col } from "react-bootstrap";
+import ProductList from "./components/ProductList";
+import ShoppingCart from "./components/ShoppingCart";
+import "./App.css";
 
 class App extends Component {
   render() {
